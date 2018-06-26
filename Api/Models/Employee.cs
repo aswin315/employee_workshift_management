593eb63a9b1c571150818b08f0afe4aa501a64f6
Aswin Unnikrishna pillai <aswin315@gmail.com>
2018-06-26 22:51:21 +0100

    Mapped tables to models and set ef context

---
+++ Api/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Models
+{
+    public class Employee
+    {
+        [Column("Employee_ID")]
+        public int EmployeeID { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        public ICollection<EmployeeWorksShift> EmployeeWorksShifts { get; set; }
+    }
+}
---
+++ Api/Models/Shift.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Models
+{
+    public class Shift
+    {
+        [Column("Shift_ID")]
+        public int ShiftID { get; set; }
+
+        [Column("Shift_Start")]
+        public DateTime ShiftStart { get; set; }
+
+        [Column("Shift_End")]
+        public DateTime ShiftEnd { get; set; }
+
+        [Column("Shift_Name")]
+        [StringLength(50)]
+        public string ShiftName { get; set; }
+
+        public ICollection<EmployeeWorksShift> EmployeeWorksShifts { get; set; }
+    }
+}
