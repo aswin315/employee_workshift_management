aa8825a5fb2c7b99cdf29ebe3cf77c25a80c882a
Aswin Unnikrishna pillai <aswin315@gmail.com>
2018-06-28 22:43:08 +0100

    Refactor Presenter to use linq

--- Api/Presenters/EmployeesDataPresenter.cs
+++ Api/Presenters/EmployeesDataPresenter.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Api.Models;
 using Api.Services;
 using Api.ViewModels;
@@ -20,30 +18,27 @@ namespace Api.Presenters
         public IList<EmployeeViewModel> GetEmployeesData()
         {
             var employees = _employeeService.GetEmployees();
-            var employeesInformation = new List<EmployeeViewModel>();
 
-            if (employees.Count == 0)
-            {
-                return employeesInformation;
-            }
-            foreach(var employee in employees)
-            {
-                employeesInformation.Add(new EmployeeViewModel {ID = employee.EmployeeID, EmployeeName = EmployeeName(employee),
-                    TotalWorkingHours = WorkingHours(employee.EmployeeWorksShifts.ToList())});
-            }
-
-            return employeesInformation;
+            var employeeInformation = from employee in employees
+                    select
+                        new EmployeeViewModel
+                        {
+                            ID = employee.EmployeeID,
+                            EmployeeName = EmployeeName(employee),
+                            TotalWorkingHours = WorkingHours(employee.EmployeeWorksShifts.ToList())
+                        };
+            return employeeInformation.ToList();
         }
 
         private Dictionary<string, int> WorkingHours(List<EmployeeWorksShift> workShifts)
         {
             var workingHours = new Dictionary<string, int>();
-            foreach(var workShift in workShifts)
+            foreach (var workShift in workShifts)
             {
                 var shift = workShift.Shift;
                 var month = shift.ShiftStart.ToString("MMMM");
                 var totalShiftTime = shift.ShiftEnd.Subtract(shift.ShiftStart).Hours;
-                if(workingHours.ContainsKey(month))
+                if (workingHours.ContainsKey(month))
                 {
                     workingHours[month] += totalShiftTime;
                 }
