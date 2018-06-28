e9b7ed4408031155dd1b7b0f1ea16b910cf18f07
Aswin Unnikrishna pillai <aswin315@gmail.com>
2018-06-28 22:40:32 +0100

    Implements presenter to format data

---
+++ Api.Tests/Presenters/EmployeesDataPresenterTests.cs
@@ -0,0 +1,112 @@
+﻿using Api.Models;
+using Api.Presenters;
+using Api.Services;
+using Api.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Api.Tests.Presenters
+{
+    public class EmployeesDataPresenterTests
+    {
+        [Fact]
+        public void GetEmployeesData_WhenCalled_CallsEmployeeServiceToGetData()
+        {
+            // Arrange
+            var employees = SetupData();
+            var employeeService = new Mock<IEmployeeService>();
+            employeeService.Setup(s => s.GetEmployees()).Returns(employees);
+            var presenter = new EmployeesDataPresenter(employeeService.Object);
+
+            // Act
+            var result = presenter.GetEmployeesData();
+
+            // Assert
+            employeeService.Verify(m => m.GetEmployees(), Times.Once);
+            Assert.IsAssignableFrom<IList<EmployeeViewModel>>(result);
+        }
+
+        [Fact]
+        public void GetEmployees_WhenEmployeeServiceReturnEmptyList_ReturnsEmptyEmployeeViewModelList()
+        {
+            //Arrange
+            var employees = SetupData();
+            var employeeService = new Mock<IEmployeeService>();
+            employeeService.Setup(s => s.GetEmployees()).Returns(new List<Employee>());
+            var presenter = new EmployeesDataPresenter(employeeService.Object);
+           
+            //Act
+            var result = presenter.GetEmployeesData();
+
+            //Assert
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void GetEmployees_WhenEmployeeServiceReturnEmployees_ReturnsEmployeeViewModelList()
+        {
+            //Arrange
+            var employees = SetupData();
+            var employeeService = new Mock<IEmployeeService>();
+            employeeService.Setup(s => s.GetEmployees()).Returns(employees);
+            var presenter = new EmployeesDataPresenter(employeeService.Object);
+
+            //Act
+            var result = presenter.GetEmployeesData();
+
+            //Assert
+            var item = result[0];
+            Assert.Equal(1, item.ID);
+            Assert.Equal("First Employee", item.EmployeeName);
+            var totalWorkingHours = item.TotalWorkingHours;
+            Assert.Equal(17, totalWorkingHours["January"]);
+            Assert.Equal(9, totalWorkingHours["February"]);
+        }
+
+
+        private List<Employee> SetupData()
+        {
+            var shifts = new List<Shift>
+            {
+                new Shift { ShiftStart = new DateTime(2018, 1, 1, 8, 0, 0), ShiftEnd = new DateTime(2018, 1, 1, 16, 0, 0) },
+                new Shift { ShiftStart = new DateTime(2018, 1, 1, 7, 0, 0), ShiftEnd = new DateTime(2018, 1, 1, 16, 0, 0) },
+                new Shift { ShiftStart = new DateTime(2018, 1, 1, 7, 0, 0), ShiftEnd = new DateTime(2018, 1, 1, 16, 0, 0) },
+                new Shift { ShiftStart = new DateTime(2018, 2, 1, 7, 0, 0), ShiftEnd = new DateTime(2018, 2, 1, 16, 0, 0) }
+            };
+
+
+            var employees = new List<Employee>
+            {
+                new Employee{
+                    EmployeeID = 1,
+                    FirstName = "First",
+                    Surname = "Employee",
+                    EmployeeWorksShifts = new List<EmployeeWorksShift>
+                    {
+                        new EmployeeWorksShift {Shift = shifts[0] },
+                        new EmployeeWorksShift {Shift = shifts[2] },
+                        new EmployeeWorksShift {Shift = shifts[3] },
+
+                    }
+                },
+                 new Employee{
+                    EmployeeID = 2,
+                    FirstName = "Second",
+                    Surname = "Employee",
+                    EmployeeWorksShifts = new List<EmployeeWorksShift>
+                    {
+                        new EmployeeWorksShift {Shift = shifts[0] },
+                        new EmployeeWorksShift {Shift = shifts[2] },
+                        new EmployeeWorksShift {Shift = shifts[3] },
+
+                    }
+                }
+            };
+            return employees;
+        }
+    }
+}
---
+++ Api/Presenters/EmployeesDataPresenter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Api.Services;
+using Api.ViewModels;
+
+namespace Api.Presenters
+{
+    public class EmployeesDataPresenter : IEmployeesDataPresenter
+    {
+        private IEmployeeService _employeeService;
+
+        public EmployeesDataPresenter(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IList<EmployeeViewModel> GetEmployeesData()
+        {
+            var employees = _employeeService.GetEmployees();
+            var employeesInformation = new List<EmployeeViewModel>();
+
+            if (employees.Count == 0)
+            {
+                return employeesInformation;
+            }
+            foreach(var employee in employees)
+            {
+                employeesInformation.Add(new EmployeeViewModel {ID = employee.EmployeeID, EmployeeName = EmployeeName(employee),
+                    TotalWorkingHours = WorkingHours(employee.EmployeeWorksShifts.ToList())});
+            }
+
+            return employeesInformation;
+        }
+
+        private Dictionary<string, int> WorkingHours(List<EmployeeWorksShift> workShifts)
+        {
+            var workingHours = new Dictionary<string, int>();
+            foreach(var workShift in workShifts)
+            {
+                var shift = workShift.Shift;
+                var month = shift.ShiftStart.ToString("MMMM");
+                var totalShiftTime = shift.ShiftEnd.Subtract(shift.ShiftStart).Hours;
+                if(workingHours.ContainsKey(month))
+                {
+                    workingHours[month] += totalShiftTime;
+                }
+                else
+                {
+                    workingHours.Add(month, totalShiftTime);
+                }
+            }
+            return workingHours;
+        }
+        private string EmployeeName(Employee employee)
+        {
+            return $"{employee.FirstName} {employee.Surname}";
+        }
+    }
+}
