f4a9af644c5f3ecf4886a20998c55c852a36b440
Aswin Unnikrishna pillai <aswin315@gmail.com>
2018-07-01 14:32:27 +0100

    Added demo mvc app that consumes the api

---
+++ EmployeeApp/Services/IEmployeeDataService.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeApp.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.Services
+{
+    public interface IEmployeeDataService
+    {
+         Task<IList<EmployeeViewModel>> GetData();
+    }
+}
